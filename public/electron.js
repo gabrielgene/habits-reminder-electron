692f827fc7fee3ba865556ac13fb16170182bee6
GabrielGene <gabrielgene97@gmail.com>
2019-08-01 02:40:52 -0300

    Initial commit

--- public/electron.js
+++ public/electron.js
@@ -1,33 +1,83 @@
-const electron = require('electron');
-const app = electron.app;
-const BrowserWindow = electron.BrowserWindow;
-
+const { app, BrowserWindow, Tray, ipcMain } = require('electron');
 const path = require('path');
+
 const isDev = require('electron-is-dev');
+let mainWindow = undefined;
+let tray = undefined;
 
-let mainWindow;
+// Don't show the app in the doc
+app.dock.hide();
 
-function createWindow() {
-  mainWindow = new BrowserWindow({width: 900, height: 680});
-  mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
+app.on('ready', () => {
+  createWindow();
+  createTray();
+});
+
+const createTray = () => {
+  const icon = `${path.join(__dirname, 'icon.png')}`;
+  console.log(icon);
+  tray = new Tray(icon);
+  tray.on('click', function(event) {
+    toggleWindow();
+  });
+};
+
+const toggleWindow = () => {
+  mainWindow.isVisible() ? mainWindow.hide() : showWindow();
+};
+
+const showWindow = () => {
+  const position = getWindowPosition();
+  console.log({ position });
+  mainWindow.setPosition(position.x, position.y, false);
+  mainWindow.show();
+};
+
+const getWindowPosition = () => {
+  const windowBounds = mainWindow.getBounds();
+  const trayBounds = tray.getBounds();
+
+  // Center window horizontally below the tray icon
+  const x = Math.round(
+    trayBounds.x + trayBounds.width / 2 - windowBounds.width / 2,
+  );
+  // Position window 4 pixels vertically below the tray icon
+  const y = Math.round(trayBounds.y + trayBounds.height + 36);
+  return { x: x, y: y };
+};
+
+const createWindow = () => {
+  const icon = `${path.join(__dirname, 'icon.png')}`;
+  mainWindow = new BrowserWindow({
+    width: 420,
+    height: 460,
+    show: false,
+    frame: false,
+    fullscreenable: false,
+    resizable: false,
+    transparent: true,
+  });
+  // mainWindow.setAspectRatio();
+  mainWindow.loadURL(
+    isDev
+      ? 'http://localhost:3000'
+      : `file://${path.join(__dirname, '../build/index.html')}`,
+  );
   if (isDev) {
     // Open the DevTools.
     //BrowserWindow.addDevToolsExtension('<location to your react chrome extension>');
-    mainWindow.webContents.openDevTools();
+    // mainWindow.webContents.openDevTools();
   }
-  mainWindow.on('closed', () => mainWindow = null);
-}
-
-app.on('ready', createWindow);
+  mainWindow.on('closed', () => (mainWindow = null));
+  mainWindow.on('blur', () => {
+    if (!mainWindow.webContents.isDevToolsOpened()) {
+      // mainWindow.hide();
+    }
+  });
+};
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit();
-  }
-});
-
-app.on('activate', () => {
-  if (mainWindow === null) {
-    createWindow();
   }
 });
--- src/App.js
+++ src/App.js
@@ -1,25 +1,146 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Icon from '@material-ui/core/Icon';
+import InputBase from '@material-ui/core/InputBase';
+
+import all from './all.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    const tasks = JSON.parse(localStorage.getItem('tasks'));
+    this.state = {
+      insert: false,
+      value: '',
+      tasks: tasks || [],
+    };
+  }
+
+  handleChange = e => this.setState({ value: e.target.value });
+
+  addItem = () => {
+    const { tasks, value } = this.state;
+    tasks.unshift({
+      index: tasks.length,
+      value,
+      done: false,
+    });
+
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+    this.setState({
+      value: '',
+      tasks,
+    });
+  };
+
+  removeItem = idx => {
+    const { tasks } = this.state;
+    tasks.splice(idx, 1);
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+    this.setState({ tasks });
+  };
+
+  markToDone = idx => {
+    const { tasks } = this.state;
+    const task = tasks[idx];
+    task.done = !task.done;
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+    this.setState({ tasks });
+  };
+
   render() {
+    const { insert, value, tasks } = this.state;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+        {tasks.length > 0 ? (
+          <div>
+            {tasks.map((t, idx) => (
+              <div key={t.index}>
+                <div
+                  style={{
+                    display: 'flex',
+                    justifyContent: 'space-between',
+                    alignItems: 'center',
+                    borderBottom: '1px solid rgba(0, 0, 0, 0.12)',
+                    height: 56,
+                  }}
+                >
+                  <div style={{ display: 'flex', alignItems: 'center' }}>
+                    <div>
+                      <Icon
+                        onClick={() => this.markToDone(idx)}
+                        className="Icon-done"
+                        style={{
+                          color: '#0091EA',
+                          marginLeft: 16,
+                          marginRight: 16,
+                        }}
+                        component="div"
+                      >
+                        {t.done ? 'check_circle' : 'check_circle_outline'}
+                      </Icon>
+                    </div>
+                    <div
+                      style={{
+                        fontFamily: 'Roboto',
+                        fontSize: 16,
+                        color: 'rgba(0, 0, 0, 0.6)',
+                        textDecoration: t.done ? 'line-through' : 'none',
+                      }}
+                    >
+                      {t.value}
+                    </div>
+                  </div>
+                  <div>
+                    <Icon
+                      className="Icon-remove"
+                      fontSize="small"
+                      component="div"
+                      onClick={() => this.removeItem(idx)}
+                    >
+                      close
+                    </Icon>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        ) : (
+          <img className="App-empty" src={all} alt="logo" />
+        )}
+        {insert ? (
+          <div className="App-action">
+            <InputBase
+              style={{ marginLeft: 16, width: 900 }}
+              autoFocus
+              placeholder="Adicionar nova tarefa"
+              value={value}
+              onChange={this.handleChange}
+              inputProps={{ 'aria-label': 'naked' }}
+              onKeyPress={e => e.key === 'Enter' && this.addItem()}
+            />
+            <div
+              className="Icon-close-wrapper"
+              onClick={() => this.setState({ insert: false })}
+            >
+              <Icon className="Icon-close" fontSize="small" component="div">
+                close
+              </Icon>
+            </div>
+          </div>
+        ) : (
+          <div
+            className="App-action"
+            onClick={() => this.setState({ insert: true })}
           >
-            Learn React
-          </a>
-        </header>
+            <div className="Icon-wrapper">
+              <Icon className="Icon" fontSize="small" component="div">
+                add
+              </Icon>
+            </div>
+            <p className="App-action-text">Adicionar tarefa</p>
+          </div>
+        )}
       </div>
     );
   }
